428858a6930d637a5610502b7fad5259ff8a5d6b
pradeep <pradeep.budhathoki1234@gmail.com>
2017-10-11 11:19:59 +0545

    another commit

---
+++ QueryString/QueryString/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QueryString
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnWebform2_Click(object sender, EventArgs e)
+        {
+            //Response.Redirect("~/WebForm2.aspx?UserName=" + Server.UrlEncode(txtName.Text) + "&UserEmail=" +Server.UrlEncode(txtEmail.Text));
+            Response.Redirect("~/WebForm2.aspx?UserName=" +txtName.Text.Replace("&","%26") + "&UserEmail=" + txtEmail.Text.Replace("&","%26"));
+        }
+    }
+}
